f966b22f4f916b19e852ada84970b47f207a4262
BlakatoraST <>
2024-03-24 20:16:57 +0100

    proyecto sin base de datos

---
+++ proyecto/aplicacion/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def clientes(request):
+    return render(request, 'clientes.html')
--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for appTaller project.
+URL configuration for proyecto project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from aplicacion import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('clientes/', views.clientes, name='clientes'),
+
 ]
